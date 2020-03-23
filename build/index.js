94b6cffd5b92151af54d62239e1550f3d3fd0736
Alex <kavanokami@gmail.com>
2020-03-23 15:22:33 +0400

    feat: add sorting logic

--- build/index.js
+++ build/index.js
@@ -1,6 +1,21 @@
 "use strict";
-console.log('test');
-var logSmth = function () {
-    console.log('smth');
-};
-logSmth();
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var _a;
+        var arr = this.collection;
+        for (var i = 0; i < arr.length; i++) {
+            for (var j = 0; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    _a = [arr[j - 1], arr[j]], arr[j] = _a[0], arr[j - 1] = _a[1];
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-console.log('test');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const logSmth = () => {
-  console.log('smth');
-};
+  sort(): void {
+    const arr = this.collection;
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - i; j++) {
+        if (arr[j] < arr[j - 1]) {
+          [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+        }
+      }
+    }
+  }
+}
 
-logSmth();
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
