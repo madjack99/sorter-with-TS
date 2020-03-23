b923c7063f9ed20a627415c96cde946bbec23ceb
Alex <kavanokami@gmail.com>
2020-03-23 14:53:52 +0400

    init: project structure

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('test');
+var logSmth = function () {
+    console.log('smth');
+};
+logSmth();
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log('test');
+
+const logSmth = () => {
+  console.log('smth');
+};
+
+logSmth();
