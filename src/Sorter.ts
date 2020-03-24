f29d75a158582391f0886207f0c925b52c2ba83b
Alex <kavanokami@gmail.com>
2020-03-24 15:10:48 +0400

    feat: change Sorter to abstract class

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -4,15 +4,17 @@ interface Sortable {
   swap(right: number, left: number): void;
 }
 
-export class Sorter {
-  constructor(public collection: Sortable) {}
+export abstract class Sorter {
+  abstract compare(left: number, right: number): boolean;
+  abstract length: number;
+  abstract swap(left: number, right: number): void;
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charCollection);
+// const charCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charCollection);
+// sorter.sort();
+// console.log(charCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charCollection.data);
+linkedList.print();
